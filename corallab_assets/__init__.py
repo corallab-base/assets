7cc5914ceab5644a59d15db81662f3c72ca1da0a
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-03-16 15:26:41 -0400

    Remove old importlib resources method and bump version

--- corallab_assets/__init__.py
+++ corallab_assets/__init__.py
@@ -6,13 +6,6 @@ def get_resource_path(path):
     else:
         result = None
 
-        ## ONE METHOD:
-        # path_parts = path.split("/")
-        # module = ".".join(["corallab_assets"] + path_parts[:-1])
-        # f = path_parts[-1]
-        # with importlib.resources.path(module, f) as f:
-        #     result = f
-
         with importlib.resources.path("corallab_assets", "") as f:
             result = f
 
